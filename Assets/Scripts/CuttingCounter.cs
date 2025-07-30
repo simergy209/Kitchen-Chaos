433b0e150621a459fecaa8de6b6d0a76bf1b87bf
simerRight <simer@DESKTOP-Q079AGT>
2025-07-30 18:21:56 +0300

    cutting recipe SO

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -8,10 +8,13 @@ public class CuttingCounter : BaseCounter
 
     public override void Interact(PlayerController player)
     {
-        //There is no kitchenObject on the clearCounter and the player carrying one, then place it on the clearCounter
-        if (!HasKitchenObject() && player.HasKitchenObject())
-            player.GetKitchenObject().SetKitchenObjectParent(this);
-
+        //There is no kitchenObject on the clearCounter and the player carrying one that it is a cutKitchenObjectSO,
+        //then place it on the clearCounter
+        if (!HasKitchenObject() && player.HasKitchenObject()) {
+            if (GetOutputForInput(player.GetKitchenObject().GetScriptableKitchenObjects()) != null) {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+        }
         //There is a kitchenObject on the clearCounter and the player is not carrying anything,then give it to the player
         else if (HasKitchenObject() && !player.HasKitchenObject())
             GetKitchenObject().SetKitchenObjectParent(player);
